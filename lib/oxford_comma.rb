3ecc891e37dc829a8ec262eabf39f7424770c2e1
Jason Pitts <juniorjr083@gmail.com>
2018-06-07 01:48:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,9 @@ if array.length == 1
 elsif array.length == 2
  array.join(" and ")
  
- else 
-   array.insert(-3, "and ").join(", ")
+ elsif  !array[-1]
+ array.insert(-2, "and ").join(", ")
+   
    
 end 
  
